5734b6394d512b4528c10e2762f0340c8e43f059
sant raj <sant2634raj@gmail.com>
2017-12-31 16:03:08 +0530

    stage 1

---
+++ shoppingbackend/src/main/java/com/firstbuy/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.firstbuy.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
